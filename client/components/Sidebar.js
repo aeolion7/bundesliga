aeaa49c67edf1791b14409872fe406d29e635524
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-14 13:29:35 -0500

    added dropdown selectors

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -5,9 +5,23 @@ const Sidebar = () => {
   return (
     <div id="sidebar">
       <img id="bundesliga-logo" src="logos/bundesliga.png" />
-      <div>
-        <h2>Matchday 13</h2>
-        <h4>Season 2018-19</h4>
+      <div id="selectors">
+        <h3>
+          <>
+            Season
+            <select>
+              <option>2018-19</option>
+            </select>
+          </>
+        </h3>
+        <h3>
+          <>
+            Matchday
+            <select>
+              <option>13</option>
+            </select>
+          </>
+        </h3>
       </div>
       <hr />
       <Table />
