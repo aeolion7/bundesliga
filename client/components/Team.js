441da0fb053bbdfe2261e4aa5bcc7bdd4fbb12b6
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-15 11:49:55 -0500

    created separate utility function module

--- client/components/Team.js
+++ client/components/Team.js
@@ -1,11 +1,11 @@
-/* eslint-disable complexity */
 import React from 'react';
+import utils from '../../server/utils';
 
 const Team = props => {
   return (
     <div className="team-info">
       <img
-        id={checkForId(props.team.name)}
+        id={utils.checkForId(props.team.name)}
         className="team-logo"
         src={props.team.logo}
       />
@@ -13,47 +13,5 @@ const Team = props => {
     </div>
   );
 };
-
-// checkForId checks each team name as provided through data
-// and if it matches one of the given team names, custom CSS
-// styling is applied to that team's logo
-
-function checkForId(teamName) {
-  // don't run the switch if the team isn't in it
-  if (
-    teamName === 'Bayer 04 Leverkusen' ||
-    teamName === 'Borussia Mönchengladbach' ||
-    teamName === 'FC Augsburg' ||
-    teamName === 'RB Leipzig' ||
-    teamName === 'TSV 1899 Hoffenheim' ||
-    teamName === 'SC Freiburg' ||
-    teamName === 'SV Werder Bremen' ||
-    teamName === 'VfB Stuttgart'
-  ) {
-    return generateId(teamName);
-  }
-}
-
-function generateId(teamName) {
-  switch (teamName) {
-    case 'Bayer 04 Leverkusen':
-      return 'leverkusen';
-    case 'Borussia Mönchengladbach':
-      return 'gladbach';
-    case 'FC Augsburg':
-      return 'augsburg';
-    case 'RB Leipzig':
-      return 'rb-leipzig';
-    case 'TSV 1899 Hoffenheim':
-      return 'hoffenheim';
-    case 'SC Freiburg':
-      return 'freiburg';
-    case 'SV Werder Bremen':
-      return 'bremen';
-    case 'VfB Stuttgart':
-      return 'stuttgart';
-    default:
-  }
-}
 
 export default Team;
---
+++ server/utils.js
@@ -0,0 +1,49 @@
+/* eslint-disable complexity */
+
+// utility functions
+
+const utils = {};
+
+// checkForId checks each team name as provided through data
+// and if it matches one of the given team names, an ID and custom CSS
+// styling is applied to that team's logo via React component
+
+utils.checkForId = teamName => {
+  // don't run the switch if the team isn't in it
+  if (
+    teamName === 'Bayer 04 Leverkusen' ||
+    teamName === 'Borussia Mönchengladbach' ||
+    teamName === 'FC Augsburg' ||
+    teamName === 'RB Leipzig' ||
+    teamName === 'TSV 1899 Hoffenheim' ||
+    teamName === 'SC Freiburg' ||
+    teamName === 'SV Werder Bremen' ||
+    teamName === 'VfB Stuttgart'
+  ) {
+    return utils.generateId(teamName);
+  }
+};
+
+utils.generateId = teamName => {
+  switch (teamName) {
+    case 'Bayer 04 Leverkusen':
+      return 'leverkusen';
+    case 'Borussia Mönchengladbach':
+      return 'gladbach';
+    case 'FC Augsburg':
+      return 'augsburg';
+    case 'RB Leipzig':
+      return 'rb-leipzig';
+    case 'TSV 1899 Hoffenheim':
+      return 'hoffenheim';
+    case 'SC Freiburg':
+      return 'freiburg';
+    case 'SV Werder Bremen':
+      return 'bremen';
+    case 'VfB Stuttgart':
+      return 'stuttgart';
+    default:
+  }
+};
+
+export default utils;
