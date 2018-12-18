463fdfa21d6286114117e5df17e34285c42d58c1
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-18 16:56:36 -0500

    dynamic API calls

--- client/reducer.js
+++ client/reducer.js
@@ -2,19 +2,7 @@ import axios from 'axios';
 import 'babel-polyfill';
 import { API_TOKEN } from '../secrets';
 
-const CHANGE_MATCHDAY = 'CHANGE_MATCHDAY';
-const CHANGE_SEASON = 'CHANGE_SEASON';
 const GOT_GAMES = 'GOT_GAMES';
-
-export const changeMatchday = matchday => ({
-  type: CHANGE_MATCHDAY,
-  matchday,
-});
-
-export const changeSeason = season => ({
-  type: CHANGE_SEASON,
-  season,
-});
 
 const gotGames = games => ({
   type: GOT_GAMES,
@@ -31,22 +19,16 @@ export const getGamesFromAPI = (seasonId, matchdayId) => {
         },
       }
     );
-    console.log(data);
+    dispatch(gotGames(data));
   };
 };
 
 const initialState = {
   games: [],
-  selectedMatchday: '',
-  selectedSeason: '',
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case CHANGE_MATCHDAY:
-      return { ...state, selectedMatchday: action.matchday };
-    case CHANGE_SEASON:
-      return { ...state, selectedSeason: action.season };
     case GOT_GAMES:
       return { ...state, games: action.games };
     default:
