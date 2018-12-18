dafa1663b0655d78316ec51142a99b6ccd673519
Joseph Lionarons <lionsamurai@gmail.com>
2018-12-18 14:04:18 -0500

    made SelectorForm component stateful

--- client/components/SelectorForm.js
+++ client/components/SelectorForm.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SelectorForm = () => {
-  return (
-    <div id="selectors">
-      <div>
-        <>
-          Season
-          <select>
-            <option>2018-19</option>
-          </select>
-        </>
-      </div>
-      <div>
-        <>
-          Matchday
-          <select>
-            <option>13</option>
-          </select>
-        </>
-      </div>
-      <button type="submit" />
-    </div>
-  );
-};
+export default class SelectorForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedMatchday: '',
+      selectedSeason: '',
+    };
+  }
 
-export default SelectorForm;
+  render() {
+    return (
+      <div id="selectors">
+        <div>
+          <>
+            Season
+            <select>
+              <option value="2017">2017-18</option>
+              <option value="2018">2018-19</option>
+            </select>
+          </>
+        </div>
+        <div>
+          <>
+            Matchday
+            <select>
+              <option value="13">13</option>
+            </select>
+          </>
+        </div>
+        <button type="submit" />
+      </div>
+    );
+  }
+}
